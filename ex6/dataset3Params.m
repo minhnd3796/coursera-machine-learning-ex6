35b7c84fec3d953aad0221037cd5e68974d2e03c
Minh Nguyen <gordonnguyen3796@gmail.com>
2017-09-08 21:28:52 +0700

    Minor Edit

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -28,7 +28,7 @@ min_sigma = 0.01;
 
 for C = [0.01 0.03 0.1 0.3 1 3 10 30]
     for sigma = [0.01 0.03 0.1 0.3 1 3 10 30]
-        model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma))
         predictions = svmPredict(model, Xval);
         error_ = mean(double(predictions ~= yval));
         if (error_ < min_error)
